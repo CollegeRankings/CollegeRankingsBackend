91fffcb2a8236402baabf1fa69d8d42194515b8a
nitinsandiego <nitinsandiego@gmail.com>
2023-10-07 08:51:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
+import threading
+
 # import "packages" from flask
+from flask import render_template  # import render_template from "public" flask libraries
+
 # import "packages" from "this" project
 from __init__ import app,db  # Definitions initialization
+from model.jokes import initJokes
+from model.users import initUsers
+from model.players import initPlayers
+
+
 # setup APIs
+from api.covid import covid_api # Blueprint import api definition
+from api.joke import joke_api # Blueprint import api definition
+from api.user import user_api # Blueprint import api definition
 from api.HelloWorld import helloworld_api # Blueprint import api definition
-from flask import render_template
+from api.player import player_api
+
+
+# setup App pages
+from projects.projects import app_projects # Blueprint directory import projects definition
+
 
 # Initialize the SQLAlchemy object to work with the Flask app instance
 db.init_app(app)
 
 # register URIs
+app.register_blueprint(joke_api) # register api routes
+app.register_blueprint(covid_api) # register api routes
+app.register_blueprint(user_api) # register api routes
+app.register_blueprint(player_api)
+app.register_blueprint(app_projects) # register app pages
 app.register_blueprint(helloworld_api) # register api routes
 
 
@@ -27,7 +49,9 @@ def table():
 
 @app.before_first_request
 def activate_job():  # activate these items 
-    pass
+    initJokes()
+    initUsers()
+    initPlayers()
 
 # this runs the application on the development server
 if __name__ == "__main__":

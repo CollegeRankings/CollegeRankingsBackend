8997ce27b558c8f3bd942dba617940e2129008e4
akshaynagesh <akshayn1219@gmail.com>
2023-10-20 09:21:17 -0700

    added skeleton for ml backend

--- api/college.py
+++ api/college.py
@@ -20,6 +20,13 @@ class colleges:
         def get(self):
             college = db.session.query(College).filter(College.id == int(request.args.get("id"))).first()
             return jsonify(college.alldetails())
+        
+    class _getrankingsCollege(Resource):
+        def get(self):
+            colleges = db.session.query(College).filter(College.ranking >= 3, College.ranking <= 5).limit(5)
+            return jsonify([college.fewdetails() for college in colleges])
+
     
     api.add_resource(_getColleges, "/colleges")
     api.add_resource(_getcollegedetails, "/collegedetails")
+    api.add_resource(_getrankingsCollege, "/mlrecommendation")
